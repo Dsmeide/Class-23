6174a8414462cc8b8b05497ff90a33e01be609b9
Dane Meide <dsmeide@gmail.com>
2024-03-10 23:05:05 -0500

    book api with local storage

--- api-template/js/main.js
+++ api-template/js/main.js
@@ -1,24 +1,28 @@
 //Example fetch using pokemonapi.co
 document.querySelector('button').addEventListener('click', getFetch)
 
+
+
+  //example book - 9780140328721, 1387207776
+
 function getFetch(){
   const choice = document.querySelector('input').value
   console.log(choice)
-  const url = `https://api.nasa.gov/planetary/apod?api_key=iJy0Nd4wZZzfW2HFgBkNV4DDUsZ6PxL3RqQRgFfI&date=${choice}`
-
+  const url = `https://openlibrary.org/isbn/${choice}.json`
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
-        console.log(data)
-        if(data.media_type === "image"){
-          document.querySelector('img').src = data.hdurl
-        }else if(data.media_type === 'video'){
-          document.querySelector('iframe').src = data.url
-        }else{
-          alert('Media Not Supported - Contact NASA IMMEDIATLY')
+
+        if(!localStorage.getItem('books')){
+          localStorage.setItem('books', data.title)
+        } else {
+          let books = localStorage.getItem('books') +', '+ data.title;
+          localStorage.setItem('books', books);
         }
-       
-        document.querySelector('h3').innerText = data.explanation
+
+        document.querySelector('h2').innerText = localStorage.getItem('books');
+        console.log(data.title)
+    
       })
       .catch(err => {
           console.log(`error ${err}`)
